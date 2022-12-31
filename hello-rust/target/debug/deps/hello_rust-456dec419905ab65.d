42ad9af006040625d2cfaeee0b0a9cbb420a415a
Jaz <pjaziel723@gmail.com>
2022-12-30 23:59:21 -0600

    try Rust Proyects

---
+++ hello-rust/target/debug/deps/hello_rust-456dec419905ab65.d
@@ -0,0 +1,5 @@
+/home/jaz/workspace/Rust/Curso_Rust/hello-rust/target/debug/deps/hello_rust-456dec419905ab65: src/main.rs
+
+/home/jaz/workspace/Rust/Curso_Rust/hello-rust/target/debug/deps/hello_rust-456dec419905ab65.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/jaz/workspace/Rust/Curso_Rust/hello-rust/target/debug/hello-rust: /home/jaz/workspace/Rust/Curso_Rust/hello-rust/src/main.rs
