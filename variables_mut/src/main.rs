a09b478ac0d2b8679ee456675166a2d077021511
Jaz <pjaziel723@gmail.com>
2023-01-05 02:10:17 -0600

    udatee variables

--- variables_mut/src/main.rs
+++ variables_mut/src/main.rs
@@ -3,5 +3,13 @@ fn main() {
  
     println!("the value of x is:{x}");
         x = 123;
-    println!("the new value of x si {x} ")
+    println!("the new value of x si {x} ");
+
+    let a = 10;
+    let a = a + 2;
+    {
+        let a = a * 2;
+        println!("the value of x in the inner scope is: {a}");
+    }
+    println!("the value of x is: {x}")
 }
